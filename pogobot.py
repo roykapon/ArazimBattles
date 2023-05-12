989f3ff6bf5a1da5e9367e1a270753c348e6facc
Ronel Lakker <r.lakker@gmail.com>
2023-05-12 12:31:18 +0300

    bank

--- pogobot.py
+++ pogobot.py
@@ -1,4 +1,6 @@
-from api import ArazimBattlesBot, EcoBloons, Exceptions, Monkeys
+from api import ArazimBattlesBot, EcoBloons, Exceptions, Monkeys, Maps
+import math
+
 
 
 class MyBot(ArazimBattlesBot):
@@ -7,7 +9,55 @@ class MyBot(ArazimBattlesBot):
     attempted_position =  5
     attempted_position_y = 5
 
+    koz = False
+    
+    bank = []
+    bank_points_prioritues = {}
+    
+    def create_bank(self):
+        for p1, p2 in self.context.get_bloon_route():
+            self.bank.append((math.floor((p1[0] + 3 * p2[0])/4), math.floor((p1[1] + 3 * p2[1])/4)))
+    
+    def update_bank(self):
+        pass
+    
+    def setup(self) -> None:
+        self.create_bank()
+    
+    def place_near_point(self, type: Monkeys, point: tuple(float, float)) -> Exception:
+        ops = [point]
+        p = ops.pop(0)
+        res = self.context.place_monkey(type, p)
+        count = 0
+        while (res != Exceptions.OK and count < 100):
+            count += 1
+            if res in [Exceptions.OUT_OF_MAP, Exceptions.TOO_CLOSE_TO_BLOON_ROUTE, Exceptions.TOO_CLOSE_TO_OTHER_MONKEY]:
+                ops.append((p[0] + 3, p[1]), (p[0] - 3, p[1]), (p[0], p[1] + 3), (p[0], p[1] - 3))
+                p = ops.pop(0)
+            else:
+                break
+
+        if res == Exceptions.OK:
+            self.monkey_count += 1
+            self.monkey_levels.append(0)
+        return res
+    
     def run(self) -> None:
+        
+        # kozim:
+        if not koz:
+            pre_last_point = self.context.get_bloon_route()[-1][0]
+            if self.context.get_map() == Maps.DUNGEON:
+                point = (240, 630)
+                res = self.context.place_monkey(Monkeys.TACK_SHOOTER, point)
+                if res == Exceptions.OK:
+                    koz = True
+            else:
+                point = pre_last_point[0] + 15, pre_last_point[1]
+                res = self.context.place_monkey(Monkeys.TACK_SHOOTER, point)
+                if res == Exceptions.OK:
+                    koz = True
+        
         if self.context.get_current_time() % 10 == 0:
             result = Exceptions.OUT_OF_MAP
             index = 0
@@ -47,3 +97,5 @@ class MyBot(ArazimBattlesBot):
             targets = self.context.get_monkey_targets(monkey_index)
             if len(targets) > 0:
                 self.context.target_bloon(monkey_index, targets[0].index)
+        
+        self.update_bank()
